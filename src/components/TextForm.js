dfaf8d718ab8b10779414aa57db4d9b8bac02a11
Adity2603 <amddeshmukh26@gmail.com>
2024-02-10 16:44:37 +0530

    Clear Button added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -33,22 +33,25 @@ export default function TextForm(props) {
           });
       }
       
-      // Example usage:
+      const TextClear = ()=>{
+        setText("")
+      }
       
       
 
-    const [text, setText] = useState('Enter text here2'); 
+    const [text, setText] = useState(''); 
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
     return (
         <div> 
             <h1>{props.heading}</h1>
             <div className="mb-3"> 
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" placeholder='Enter Your Text' value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-danger mx-2 " onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-danger mx-2" onClick={handleLoClick}>Convert to Uppercase</button>
-            <button className="btn btn-danger mx-2 ChangeName " onClick={copyTextFromTextareaToClipboard}>Copy to clipboard</button>
+            <button className="btn btn-danger mx-2" onClick={handleLoClick}>Convert to Lower Case</button>
+            <button className="btn btn-success mx-2 ChangeName " onClick={copyTextFromTextareaToClipboard}>Copy to clipboard</button>
+            <button className="btn btn-primary mx-2 ChangeName " onClick={TextClear}>Clear</button>
 
             <div className="container my-3 ">
                 <h3>Character = {text.length}</h3>
