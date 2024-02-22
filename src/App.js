9960cf2edae22b17d11483566736ee0b61284706
Adity2603 <amddeshmukh26@gmail.com>
2024-02-22 15:35:29 +0530

    About Section added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About'
 import Alert from './components/alert';
 
 function App() {
@@ -39,6 +40,7 @@ function App() {
     </div>
     <div className="container my-3">
     <TextForm showalert= {showalert} heading="Enter the text to analyze below"/>
+    <About/>
     </div>
     </>
   );
